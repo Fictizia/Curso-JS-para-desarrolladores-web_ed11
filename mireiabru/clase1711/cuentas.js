49e1114261e1238a0495a335903d8a40619e878e
mireiabru <mireiabrumartinez@gmail.com>
2018-11-17 14:55:12 +0100

    Add js finished work

--- mireiabru/clase1711/cuentas.js
+++ mireiabru/clase1711/cuentas.js
@@ -1,8 +1,18 @@
-const trueElement = true
-const falseElement = false
+// voy a cambiar solo la edad y el país
+// la edad será un número positivo entre 0 y 150
+// el país solo puede ser Spain o USA, y se escribirá así
 
-if (trueElement && falseElement){
-    console.log('HA DADO TRUE')
-} else{
-    console.log('HA DADO FALSE')
-}
+const age = 5
+const country = 'Spain'
+
+let ageToEnter = 21
+
+    if(country === 'Spain'){
+        ageToEnter = 18
+    }
+
+    if (age >= ageToEnter){
+        console.log('Puedes entrar')
+    }else{
+        console.log('You shall not pass')
+    }
