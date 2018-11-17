2a78baa696495522e4ad75899e3d912f43b8d933
victoriasilvamora <javascript@ZiMac4-6.local>
2018-11-17 14:42:27 +0100

    algoritmoEdad/Pais

---
+++ Victoria Silva/Clase1.js
@@ -0,0 +1,22 @@
+const age = 5;
+const country ='Spain'
+
+if (country = "Spain"){
+    const hasAge = 18
+
+}else{
+    const hasAge = 21 
+}
+
+
+
+if (age<= hasAge){
+    console.log('Puede entrar en la disco');
+
+}else{
+    console.log('No Puede entrar en la disco') ;
+}
+
+
+
+
