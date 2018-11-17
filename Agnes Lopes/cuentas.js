0bb66f6923199ab6847012b1e7eb96edebeee988
alopesfernandes <agnesfclopes@gmail.com>
2018-11-17 14:21:59 +0100

    add Exercise 1.1

---
+++ Agnes Lopes/cuentas.js
@@ -0,0 +1,21 @@
+// && ||
+
+// esto que estoy explicando es como funciona el if && y el else ||
+
+const trueElement = true
+const falseElement = false
+
+
+
+if (trueElement && falseElement) {
+    console.log('HA DADO TRUE')
+} else{
+    console.log('HA DADO FALSE')
+}
+
+console.log(5 <= '5' && typeof 5 === typeof '5')
+
+
+
+
+
