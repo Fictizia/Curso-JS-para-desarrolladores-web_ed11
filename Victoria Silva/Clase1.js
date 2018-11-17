391051db374bf75e4963ea7ca3f317e93672f575
victoriasilvamora <javascript@ZiMac4-6.local>
2018-11-17 14:57:36 +0100

    final

--- Victoria Silva/Clase1.js
+++ Victoria Silva/Clase1.js
@@ -1,21 +1,19 @@
-const age = 5;
+const age = 5
 const country ='Spain'
+let hasAge = 21
 
-if (country === "Spain"){
-    const hasAge = 18
-
-}else{
-    const hasAge = 21 
-}
+    if (country === "Spain"){
+        const hasAge = 18
+    }
 
 
 
-if (age>= hasAge){
-    console.log('Puede entrar en la disco');
+    if (age>= hasAge){
+        console.log('Puede entrar en la disco')
 
-}else{
-    console.log('No Puede entrar en la disco') ;
-}
+    }else{
+        console.log('No Puede entrar en la disco') 
+    }
 
 
 
