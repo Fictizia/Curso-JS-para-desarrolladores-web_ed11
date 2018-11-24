2b081a47fe30d55ef912e23c9bc23f0b7f32adb6
alopesfernandes <agnesfclopes@gmail.com>
2018-11-24 12:38:09 +0100

    add more ex

---
+++ Agnes Lopes/complex.js
@@ -0,0 +1,30 @@
+/*let num = 1
+
+num = num + 1
+num ++
+num --
+*/
+/*let first = 1
+let second = first
+
+first = 5
+
+console.log(`first vale: ${first}`)
+console.log(`second vale: ${second}`)
+//console.log(second)
+*/
+/*let first = 'hola'
+let second = first
+
+first = 'adios'
+
+console.log(`first vale: ${first}`)
+console.log(`second vale: ${second}`)
+*/
+let first = true
+let second = first // !first negarla o 5 != 5
+
+first = !false
+
+console.log(`first vale: ${first}`)
+console.log(`second vale: ${second}`)
