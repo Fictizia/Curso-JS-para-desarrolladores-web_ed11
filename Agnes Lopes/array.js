42e925a6201b1d7a3a9ef1cf536f2237ee4181c2
alopesfernandes <agnesfclopes@gmail.com>
2018-11-24 11:50:50 +0100

    add array ex

---
+++ Agnes Lopes/array.js
@@ -0,0 +1,29 @@
+//const numberOfEachFood = [1,2,3]
+//const food = ['potatoes', 'bread' , 'eggs']
+
+/*const list = [
+                 [
+                      food[0], numberOfEachFood[0]
+                 ],
+                 [
+                       food[1], numberOfEachFood[1]
+                 ],
+                 [
+                      food[2], numberOfEachFood[2]
+                 ],
+                ]
+                */
+//array + array
+//console.log(list[1][1]) easier way
+//const barrasBread = list[1]
+//const numberBarrasBread = barrasBread[1]
+//console.log(numberBarrasBread) too much work
+
+const lista = {
+    barrasPan : 2,
+    huevo: 12,
+    berenjenas: 5
+}
+console.log(lista)
+
+
