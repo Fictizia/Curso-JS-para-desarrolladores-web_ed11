8ebc670775d657702ab939bb4929ce15f42ec453
alopesfernandes <agnesfclopes@gmail.com>
2018-11-24 13:02:29 +0100

    example

---
+++ Agnes Lopes/copyagnes.js.js
@@ -0,0 +1,30 @@
+let pupils = ['Agnes', 'Jaime', 'Maria', 'Mireia', 'Victoria']
+
+const pupilsNumberAtBegining = pupils.length
+let index = pupils.length - 1
+//const pupilsNumber = pupils.length
+
+//tHIS MUST BE A BUCLE, WE WILL SEE HOW, IN NEXT CLASS
+
+//let index = pupilsNumber - 1
+
+console.log(pupils[index])
+index--
+console.log(pupils[index])
+index--
+console.log(pupils[index])
+index--
+console.log(pupils[index])
+index--
+console.log(pupils[index])
+index--
+
+// need to empty the class, but I will need to know the total
+// of pupils we had at the begining
+
+pupils = ['Agnes']
+
+//time is elapsed, remind me, how many pupils did we have?
+
+//console.log(`this must be the total numer ${index}`)
+console.log(pupilsNumberAtBegining)
