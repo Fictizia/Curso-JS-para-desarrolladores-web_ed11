bb5869a28a2f70af2839bf6c541b849be0fa8164
josheriff <jakermater@gmail.com>
2018-11-24 14:43:33 +0100

    trukis

---
+++ ejemplos/arraysTruquis.js
@@ -0,0 +1,11 @@
+let myArray = ['Maria', 'Mireia', 'Jaime', 'Agnes', 'Victoria', 'Mireia']
+
+let otherArray = myArray.slice()
+
+console.log(otherArray)
+console.log(myArray)
+
+myArray.shift()
+
+console.log(myArray)
+console.log(otherArray)
