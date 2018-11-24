5b8959271316a426174ee5785d0f40900f80dfd6
mireiabru <mireiabrumartinez@gmail.com>
2018-11-24 14:39:31 +0100

    string exercices

---
+++ mireiabru/clase2411/strings.js
@@ -0,0 +1,13 @@
+// STRINGS
+
+const name = 'Victoria'
+const age = 35
+const doing = 'Studying'
+const goodAtMath = true
+
+let anotherText = `Hello my name is ${name}
+I am ${age} years old
+and I'm ${doing}
+`
+
+console.log(anotherText)
