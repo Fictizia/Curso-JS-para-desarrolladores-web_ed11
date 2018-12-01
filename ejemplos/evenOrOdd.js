b9816d20a60ec65e941a4bc7fe65565c8635ea51
josheriff <jakermater@gmail.com>
2018-12-01 11:12:25 +0100

    examples

---
+++ ejemplos/evenOrOdd.js
@@ -0,0 +1,11 @@
+evenOrOdd(6);    
+    
+function evenOrOdd(num) {
+    if (num % 2 === 0) {
+        console.log(`${num} is an even number`);
+    }
+    else {
+        console.log(`${num} is an odd number`);
+    }
+}
+
