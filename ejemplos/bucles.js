3ab7be4f35d422c869453d086e9a53b5c7e99e9a
josheriff <jakermater@gmail.com>
2018-12-01 12:46:28 +0100

    returns and bucles same day??!!, this pupils are so cool...

---
+++ ejemplos/bucles.js
@@ -0,0 +1,11 @@
+let numberOfTimes = 1
+while (numberOfTimes <=100) {
+    console.log(numberOfTimes)
+    numberOfTimes++
+}
+
+for (let numberOfTimes = 1; numberOfTimes <=0; numberOfTimes++) {
+    console.log(numberOfTimes)
+    
+}
+
