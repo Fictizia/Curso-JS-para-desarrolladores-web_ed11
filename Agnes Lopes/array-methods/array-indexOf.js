b0a14670c5cd449ff9a315eae670e73c31cd966a
alopesfernandes <agnesfclopes@gmail.com>
2018-12-01 12:18:39 +0100

    add new example of Array

--- Agnes Lopes/array-methods/array-indexOf.js
+++ Agnes Lopes/array-methods/array-indexOf.js
@@ -9,3 +9,20 @@ console.log(beasts.indexOf('bison', 2));
 
 console.log(beasts.indexOf('giraffe'));
 // expected output: -1
+
+const fictiziaPupils =['Maria',
+                         'Jaime',
+                         'Tere',
+                         'Karina',
+                         'Mireia',
+                         'Jaime',
+                         'Jose',
+                         'Agnes',
+                         'Jaime',
+                         'Jose',
+                         'Jaime']
+let jaimePositions = []
+jaimesPositions.push(fictiziaPupils.indexOf('Jaime'))
+//constjaimesPositions = fictiziaPupils.indexOf('Jaime')
+console.log(jaimePositions) // 1
+// fictiziaPupils[1]
