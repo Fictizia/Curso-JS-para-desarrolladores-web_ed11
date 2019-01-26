2812bad06bfd618a7e372f0cbef271ea4397fa07
josheriff <jakermater@gmail.com>
2019-01-26 13:32:43 +0100

    pass arguments in array

---
+++ arguments.js
@@ -0,0 +1,12 @@
+class MyClass{
+
+constructor(anArray){
+    this.id = anArray[0]
+    this.name = anArray[1]
+    this.surname = anArray[3]
+}
+}
+
+const siempre = [1,'Jose','Galego']
+
+list(siempre)
